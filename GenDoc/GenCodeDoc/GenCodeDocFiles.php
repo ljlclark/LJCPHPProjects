4e3683fc9506d3d82a2f05bc7c65d69111e892c4
ljlclark <35316538+ljlclark@users.noreply.github.com>
2025-05-04 04:32:42 +0800

    PHP GenCodeDoc

--- GenDoc/GenCodeDoc/GenCodeDocFiles.php
+++ GenDoc/GenCodeDoc/GenCodeDocFiles.php
@@ -1,8 +1,10 @@
 <?php
   // DocDataGenTest.php
   declare(strict_types=1);
+  // Must refer to exact same file everywhere in codeline.
   require_once "GenCodeDocLib.php";
 
+  echo("Enter: GenCodeDocFiles.php");
   $genCodeDoc = new GenCodeDoc();
   $genCodeDoc->CreateFromList();
 ?>
--- GenDoc/GenCodeDoc/GenCodeDocLib.php
+++ GenDoc/GenCodeDoc/GenCodeDocLib.php
@@ -1,24 +1,54 @@
 <?php
+  // Copyright(c) Lester J. Clark and Contributors.
+  // Licensed under the MIT License.
   // GenCodeDocLib.php
   declare(strict_types=1);
-  $devPath = "c:/Users/Les/Documents/Visual Studio 2022/LJCPHPProjects";
-  require_once "$devPath/LJCPHPCommon/LJCCommonLib.php";
-  require_once "$devPath/LJCPHPCommon/LJCTextLib.php";
-  require_once "$devPath/GenDoc/DocDataLib/LJCDocDataGenLib.php";
-  require_once "$devPath/GenDoc/GenDataLib/LJCGenDataGenLib.php";
+  $path = "../..";
+  // Must refer to exact same file everywhere in codeline.
+  require_once "$path/LJCPHPCommon/LJCTextLib.php";
+  require_once "$path/GenDoc/DocDataLib/LJCDocDataGenLib.php";
+  include_once "$path/GenDoc/DocDataLib/LJCDebugLib.php";
+  require_once "$path/GenDoc/GenDataLib/LJCGenDataGenLib.php";
+  include_once "../DocDataLib/LJCDebugLib.php";
 
+  // Classes
+  // File
+  //   GenCodeDoc
+
+  // Contains classes to generate HTML doc for files listed
+  // in GenCodeSourceFileList.txt 
+
+  // Calling Code
+  // GenCodeDocFiles.php
+
+  // Main Call Tree
+  // CreateFromList() public
+  //   CreateFilePages() public
+  //     LJCDocDataGen->CreateDocDataXMLString()
+  //     LJCGenDataGen->CreateLibXMLString()
+
+  // ***************
   class GenCodeDoc
   {
     /// <summary>Initializes an object instance.</summary>
     public function __construct()
     {
+      // Instantiate properties with Pascal case.
+      $isEnabled = false;
+      $this->Debug = new LJCDebug("GenCodeDocLib", "GenCodeDoc"
+        , $isEnabled);
+      $this->Debug->IncludePrivate = true;
+
       $this->DocDataGen = new LJCDocDataGen();
       $this->GenDataGen = new LJCGenDataGen();
-    }
+    } // __construct()
 
     /// <summary>Creates CodeDoc pages from source file list.</summary>
     public function CreateFromList()
     {
+      $this->Debug->WriteStartText("CreateFromList");
+
+      // Gets the list of files to read.
       $sourceFileListSpec = "GenCodeSourceFileList.txt";
       $inputStream = fopen($sourceFileListSpec, "r+");
       while(false == feof($inputStream))
@@ -26,14 +56,17 @@
         $line = (string)fgets($inputStream);
         $this->CreateFilePages($line);
       }
+
+      $this->Debug->AddIndent(-1);
     }
 
     /// <summary>Create the HTML files.</summary>
     /// <param name="$fileSpecLine">The source file spec.</param>
     public function CreateFilePages($fileSpecLine) : void
     {
-      global $webCommonPath;
-      global $devPath;
+      $this->Debug->WriteStartText("CreateFilePages");
+
+      global $path;
       $writeDocDataXML = false;
       $writeGenDataXML = false;
 
@@ -42,17 +75,18 @@
       {
         switch ($tokens[0])
         {
-          case "webCommonPath":
-            $fileSpec = "$webCommonPath";
-            break;
-          case "devPath":
-            $fileSpec = "$devPath";
+          case "path":
+            // *** Change *** 5/2/25
+            //$fileSpec = "$devPath";
+            $fileSpec = "$path";
             break;
           default:
             $fileSpec = $tokens[0];
             break;
         }
         $fileSpec .= trim($tokens[1]);
+        // **********
+        $this->Debug->Write("fileSpec = $fileSpec");
 
         $docXMLString = $this->DocDataGen->CreateDocDataXMLString($fileSpec
           , $writeDocDataXML);
@@ -62,6 +96,8 @@
             , $fileSpec, $writeGenDataXML);
         }
       }
+
+      $this->Debug->AddIndent(-1);
     }
 
     // The Generate DocData XML object.
